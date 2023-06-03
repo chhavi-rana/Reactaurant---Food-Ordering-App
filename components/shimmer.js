5bb66e99829d4300476dcbcc1c77156063213ec1
= <chhavirana83@gmail.com>
2023-06-03 17:23:42 +0530

    Add shimmer effect

---
+++ components/shimmer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles.css";
+
+const Shimmer = () => {
+  return (
+    <>
+      <div className="shimmer-header"></div>
+      <div className="shimmer-container">
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+        <div className="shimmer-card"></div>
+      </div>
+    </>
+  );
+};
+
+export default Shimmer;
