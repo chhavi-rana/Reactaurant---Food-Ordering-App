a60442962ad5260bbbacd1aace6ee4e7102f442a
Chhavi Rana <83640180+chhavi-rana@users.noreply.github.com>
2023-06-04 02:08:45 +0530

    Update body.js

--- components/body.js
+++ components/body.js
@@ -43,7 +43,9 @@ const Body = () => {
 
   async function getRestaurants() {
     const response = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING",{
+      mode: 'no-cors'
+ }
     );
     const json = await response.json();
     console.log(json);
