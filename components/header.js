e826868114931f227b8aa304cf7c8e933da420ed
= <chhavirana83@gmail.com>
2023-06-08 17:11:33 +0530

    using Link tags for navigation

--- components/header.js
+++ components/header.js
@@ -1,28 +1,36 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [loggedInUser, setLoggedInUser] = useState(false);
   return (
     <div className="nav-bar">
       <div className="logo">
-        <a href="/">
+        <Link to="/">
           <img src={require("../images/logo.png")} alt="logo" />
-        </a>
+        </Link>
       </div>
       <div className="nav-link">
         <ul>
-          <li className="nav-items">Offers</li>
-          <li className="nav-items">Help</li>
-          <li className="nav-items">Sign In</li>
-          <li className="nav-items">Cart</li>
-          {loggedInUser? (
-          <button className="auth" onClick={() => setLoggedInUser(false)}>
-            LogOut
-          </button>
-          ): (
-          <button className="auth" onClick={() => setLoggedInUser(true)}>
-            LogIn
-          </button>
+          <Link to="/offer">
+            <li className="nav-items">Offers</li>
+          </Link>
+          <Link to="/help">
+            <li className="nav-items">Help</li>
+          </Link>
+          {/* <li className="nav-items">Sign In</li> */}
+          <Link to="/cart">
+            <li className="nav-items">Cart</li>
+          </Link>
+
+          {loggedInUser ? (
+            <button className="auth" onClick={() => setLoggedInUser(false)}>
+              LogOut
+            </button>
+          ) : (
+            <button className="auth" onClick={() => setLoggedInUser(true)}>
+              LogIn
+            </button>
           )}
         </ul>
       </div>
