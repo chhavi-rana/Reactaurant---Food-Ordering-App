7860a2293d226c93ee0be280c92d28c0cc7fd7d6
= <chhavirana83@gmail.com>
2023-06-26 09:26:54 +0530

    Updated Cart component to show added items, remove items, and clear cart

--- components/cart.js
+++ components/cart.js
@@ -1,8 +1,42 @@
+import React, { useContext } from "react";
+import { CartContext } from "./CartContext";
+// import './CartComponent.css
+
 const Cart = () => {
+  const { cartItems, removeFromCart, clearCart } = useContext(CartContext);
+
   return (
-    <div className="cart">
-      <h1> Your Cart</h1>
-      <h2 className="empty">Empty</h2> 
+    <div className="cart-container">
+      <h2 className="cart-title">Cart</h2>
+
+      {cartItems.length > 0 ? (
+        <div>
+          <ul>
+            {cartItems.map((item) => (
+              <li className="cart-item" key={item.id}>
+                <div className="item-info">
+                  <div className="item-icon">
+                    {/* Add your item icon here */}
+                  </div>
+                  <div className="item-name">{item.name}</div>
+                </div>
+                <div className="item-price">{item.price}</div>
+                <button
+                  className="remove-button"
+                  onClick={() => removeFromCart(item.id)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button className="clear-button" onClick={clearCart}>
+            Clear Cart
+          </button>
+        </div>
+      ) : (
+        <p>Your cart is empty.</p>
+      )}
     </div>
   );
 };
