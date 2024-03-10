045a3799b76a4f4697252c80bea1d02aacb50d8e
Chhavi Rana <83640180+chhavi-rana@users.noreply.github.com>
2024-03-10 14:10:13 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -6,7 +6,7 @@ export const IMG_CDN_URL =
 export const ITEM_IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
-// Swiggy API to get Restaurant data with corsproxy
+// Swiggy API to get Restaurant data with corsproxy (corsproxy no longer working)
 export const swiggy_api_URL =
   "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
